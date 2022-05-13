4f7f66d75a4d74fad9287c05fcbda7be724a7a93
Thowhid31 <thowhidantor16@gmail.com>
2022-05-13 16:10:27 +0600

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-      <h1 className='text-red-500'>Clean Co</h1>
-      <button className='btn btn-primary'>Hello</button>
+     <Navbar/>
     </div>
   );
 }
