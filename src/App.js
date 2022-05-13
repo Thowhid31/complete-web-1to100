0e41e384f9aa9297ef726448ba300f024f67ced7
Thowhid31 <thowhidantor16@gmail.com>
2022-05-13 16:28:43 +0600

    added nav items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
 import Home from './Pages/Home';
+import Login from './Pages/Login';
 
 function App() {
   return (
     <div>
      <Navbar>
      <Home/>
-
+      <About/>
+      <Contact/>
+      <Login/>
      </Navbar>
      
     </div>
