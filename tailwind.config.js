bc10a61fed6ba03479159090123ca0982a538cf8
Thowhid31 <thowhidantor16@gmail.com>
2022-05-13 18:25:49 +0600

    added navbar toggle for dark

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,27 +3,28 @@ module.exports = {
   daisyui: {
     themes: [
       {
-        mytheme: {
-        
-"primary": "#176F6B",
-        
-"secondary": "#FFC000",
-        
-"accent": "#FFEDD5",
-        
-"neutral": "#F3F4F6",
-        
-"base-100": "#FFFFFF",
-        
-"info": "#98A8DD",
-        
-"success": "#1BBB70",
-        
-"warning": "#DF7E07",
-        
-"error": "#FA5C5C",
+        light: {
+
+          "primary": "#176F6B",
+
+          "secondary": "#FFC000",
+
+          "accent": "#FFEDD5",
+
+          "neutral": "#F3F4F6",
+
+          "base-100": "#FFFFFF",
+
+          "info": "#98A8DD",
+
+          "success": "#1BBB70",
+
+          "warning": "#DF7E07",
+
+          "error": "#FA5C5C",
         },
       },
+      "dark",
     ],
   },
   plugins: [require("daisyui")],
