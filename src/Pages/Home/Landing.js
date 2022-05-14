0e7b6c093a3310aa416ff6aacf13dd8c5dde5663
Thowhid31 <thowhidantor16@gmail.com>
2022-05-14 18:31:41 +0600

    added input format

--- src/Pages/Home/Landing.js
+++ src/Pages/Home/Landing.js
@@ -3,7 +3,8 @@ import heroimg from '../../assets/images/bucketgirl.png'
 
 const Landing = () => {
     return (
-        <div class="hero h-screen lg:h-[60vh] bg-accent mt-16">
+        <>
+        <div class="hero h-screen lg:h-[60vh] bg-accent relative z-0 mt-16">
   <div class="hero-content flex-col lg:flex-row">
     
     <div>
@@ -17,6 +18,20 @@ const Landing = () => {
     </div>
   </div>
 </div>
+<div className='rounded-2xl mx-auto shadow-lg relative z-20 p-10 w-5/6 bg-base-100'>
+    <h1>Get Free Estimate</h1>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+    <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+    <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+    <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+    <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+    <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+    <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+    <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+    <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+    </div>
+</div>
+</>
     );
 };
 
