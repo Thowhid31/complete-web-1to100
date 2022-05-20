c570d1f7a2347a0787a74fe1387ac4d37fbb601b
Thowhid31 <thowhidantor16@gmail.com>
2022-05-20 11:04:12 +0600

    implement fatcher and on client

--- src/Pages/Dashboard/AddService.js
+++ src/Pages/Dashboard/AddService.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import facther from '../../api';
 
 const AddService = () => {
+    const { register, handleSubmit, reset } = useForm();
+
+    const onSubmit = async (data) => {
+        const res = await facther.post('add-service', data);
+        console.log(res);
+        reset()
+
+    }
+
     return (
-        <div>
-            <h1>Add your Service</h1>
+        <div className='h-screen w-full flex justify-center items-center bg-accent'>
+            <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div class="card-body">
+                    <h1 className='text-2xl text-center'>Add Service</h1>
+
+                    <form onSubmit={handleSubmit(onSubmit)}>
+
+                    
+
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text">Service Name</span>
+                        </label>
+                        <input type="text"  class="input input-bordered" 
+                        {...register("serviceName")}
+                        />
+                    </div>
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text">Service Charge</span>
+                        </label>
+                        <input type="text" class="input input-bordered" 
+                        {...register("serviceCharge")}
+                        />
+                        
+                    </div>
+                    <div class="form-control mt-6">
+                        <button type='submit' class="btn btn-primary" >Add Service</button>
+                    </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
